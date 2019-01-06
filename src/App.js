58b533e5b5bc7aee676b15411cb9d897874661d8
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-06 19:57:25 +0530

    create a state for storing whether a user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from '@firebase/app';
+import '@firebase/auth';
 import { Header } from './components/common';
 import {
     firebaseApiKey,
@@ -13,6 +14,8 @@ import {
 import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: false };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: firebaseApiKey,
@@ -21,6 +24,14 @@ class App extends Component {
             projectId: firebaseProjectId,
             storageBucket: firebaseStorageBucket,
             messagingSenderId: firebaseMessagingSenderId
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
         });
     }
 
