50e0ad42941a962e535d573021b5e32c4ee711b2
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2019-01-06 18:50:19 +0530

    password input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '' };
+    state = { email: '', password: '' };
 
     render() {
         return (
@@ -11,12 +11,21 @@ class LoginForm extends Component {
                     <Input
                         label="Email"
                         placeholder="user@gmail.com"
+                        secureTextEntry={false}
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
 
-                <CardSection />
+                <CardSection>
+                    <Input
+                        label="Password"
+                        placeholder="password"
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
